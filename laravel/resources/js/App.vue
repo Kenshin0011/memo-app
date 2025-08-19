637e51bc4e4be732734ce51c2bb3a426651ddf3f
kenshin0011 <nakanishi@social-db.co.jp>
2025-08-19 20:22:33 +0900

    first commit

---
+++ laravel/resources/js/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>Hello from Vue 🎉</h1>
+    <router-view/>
+  </div>
+</template>
---
+++ laravel/resources/views/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Vue App</title>
+    @vite(['resources/js/app.ts', 'resources/css/app.css'])
+</head>
+<body>
+<div id="app"></div>
+</body>
+</html>
