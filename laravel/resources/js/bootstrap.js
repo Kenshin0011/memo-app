a1c688d3c8a958054814abe557ca4194923d0d7a
kenshin0011 <nakanishi@social-db.co.jp>
2025-08-20 01:21:34 +0900

    fix: api連携に必要な設定を追加

--- laravel/resources/js/bootstrap.js
+++ laravel/resources/js/bootstrap.js
@@ -1,4 +1,14 @@
 import axios from "axios";
+
 window.axios = axios;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_APP_URL + "/api",
+  headers: {
+    "X-Requested-With": "XMLHttpRequest",
+  },
+});
+
+export default api;
