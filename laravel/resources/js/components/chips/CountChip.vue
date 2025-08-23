ca3d606351d24286a1029596955b16683b99f438
kenshin0011 <nakanishi@social-db.co.jp>
2025-08-23 13:42:14 +0900

    feat: Add Memo CountChip

---
+++ laravel/resources/js/components/chips/CountChip.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+type Props = {
+  count: number;
+};
+
+defineProps<Props>();
+</script>
+
+<template>
+  <span class="text-sm text-gray-500 bg-orange-100 px-3 py-1 rounded-full"> {{ count }}件 </span>
+</template>
