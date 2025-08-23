f3623a637488e729c18b510a3664c4b9baceb57b
kenshin0011 <nakanishi@social-db.co.jp>
2025-08-23 12:49:18 +0900

    feat: Add MemoList title

---
+++ laravel/resources/js/features/memos/components/MemoList.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import SvgTextHeading from "../../../components/texts/SvgTextHeading.vue";
+import DocumentSvg from "../../../components/svgs/DocumentSvg.vue";
+</script>
+
+<template>
+  <SvgTextHeading :icon="DocumentSvg" text="保存されたメモ" />
+</template>
